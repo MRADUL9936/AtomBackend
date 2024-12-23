cf6fc9ab8d85ed90b28d011b66070fb7ca05ecab
Mradul <mradul9936@gmail.com>
2024-12-23 23:39:13 +0530

    "cors url"

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ import './jobs/cronJob.jobs.js'
 
 dotenv.config()
 const app=express()
-const whitelist = ['http://localhost:5173','atom.mradulvishwakarma.com'];
+const whitelist = ['http://localhost:5173','https://atom.mradulvishwakarma.com'];
 
 // Configure the CORS middleware
 app.use(cors({
